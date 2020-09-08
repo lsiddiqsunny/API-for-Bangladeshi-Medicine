221ea254ea5d702e4a5fa31f974d946180d9f99f
Latif siddiq sunny <lsiddiqsunny@gmail.com>
2020-09-08 07:52:28 +0600

    Add search by name

--- Medinice_APIs/index.js
+++ Medinice_APIs/index.js
@@ -5,6 +5,30 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var str = "";
 
+
+app.route('/search/name').get(function(req, res) {
+    MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+
+        function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("MedicineDB");
+            var Name = req.query.name;
+            dbo.collection("Medicine").find({ 'Name': new RegExp(Name, 'i') }).toArray(function(err, result) {
+                if (err) throw err;
+                for (var i = 0; i < result.length; i++) {
+                    str = str + "  Medicine Name:  " + result[i].Name + "</br>";
+                }
+                res.send(str);
+                db.close();
+            });
+        });
+
+
+});
+
 app.route('/getAll').get(function(req, res) {
 
     MongoClient.connect(url, {
